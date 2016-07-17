852e601f5e0ae6be2c29f5c9389e50c0bcf1e9ec
Taylor Savage <tjsavage@google.com>
2016-07-16 17:39:25 -0700

    now can log to firebase

--- apps/Base.js
+++ apps/Base.js
@@ -4,7 +4,6 @@ var firebase = require('firebase');
 
 class Base {
   constructor(config) {
-    console.log(config);
     this._config = config;
 
     firebase.initializeApp(this._config.firebase);
@@ -14,12 +13,28 @@ class Base {
   _connect() {
     this._db = firebase.database();
     this._stateRef = this._db.ref('state/' + this._config.appId);
+    this._logRef = this._db.ref('log/' + this._config.appId);
   }
 
   set state(obj) {
-    console.log('setting state', obj);
     this._stateRef.set(obj);
   }
+
+  /*
+  * Log a message to firebase.
+  * @param {string} str - The message to log
+  * @param {string} type - The type of message 'info', 'error', 'warning'. Defaults to 'info'.
+  */
+  log(str, type) {
+    var newLogRef = this._logRef.push();
+
+    newLogRef.set({
+      message: str,
+      timestamp: (new Date()).toString(),
+      type: type || "info"
+    });
+
+   }
 }
 
 module.exports = Base;
