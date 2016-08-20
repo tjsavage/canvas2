45dd61a12e76382cea1116e4c088f4ae9a15b33e
Taylor Savage <tjsavage@google.com>
2016-08-20 11:51:41 -0700

    added dashboard section

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var argv = require('yargs').argv;
 var randomstring = require('randomstring');
 
 var apps = require('./apps');
+var hubs = require('./hubs');
+var HubFactory = hubs.HubFactory;
 
 var appName = argv._[0];
 var config = argv.config;
+var localHub = argv.localHub;
 
 if (config) {
   config = JSON.parse(config);
+}
+
+if (localHub) {
+  var factory = new HubFactory();
+  var hub = factory.getHubInstance({'hubId': 'a', 'hubClass': 'LocalHub'});
 }
 
 config = Object.assign({
@@ -17,4 +25,8 @@ config = Object.assign({
   verbose: true
 }, config);
 
-apps.startApp(appName, config);
+var app = apps.startApp(appName, config);
+
+if (hub) {
+  app.registerHub(hub);
+}
