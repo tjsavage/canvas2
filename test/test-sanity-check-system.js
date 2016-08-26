7c3604d29a42ddefbfc57619a3145bbd1f8ef224
Taylor Savage <tjsavage@google.com>
2016-08-25 20:59:31 -0700

    added basic sanity check test

--- test/test-sanity-check-system.js
+++ test/test-sanity-check-system.js
@@ -3,8 +3,27 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var os = require('os');
+var systemConfig = require('../lib/system-config')
 
-describe('system-config', function() {
-  before(function() {
-    var systemConfigFilepath = process.env.CANVAS2_SYSTEM_CONFIG;
-  })
+var apps = require('../apps');
+
+describe('sanity check system-config', function() {
+  it('should be able to find the config', function() {
+    expect(systemConfig.hubs).to.not.be.null;
+    expect(systemConfig.devices).to.not.be.null;
+  });
+
+  it('should be able to start every app in the config', function() {
+    var devices = systemConfig.devices;
+    var excluded = ["PIRSensor"];
+
+    Object.keys(devices).forEach(function(deviceId) {
+      var device = devices[deviceId];
+      device.apps.forEach(function(app) {
+        if (excluded.indexOf(app.app) == -1) {
+          apps.startApp(app);
+        }
+      });
+    });
+  });
+});
